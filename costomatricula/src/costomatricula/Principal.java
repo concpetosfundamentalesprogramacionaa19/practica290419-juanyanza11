01e62949d5d6fef490d783ecd9f705bb48711a8a
juanyanza11 <ww4fjj@gmail.com>
2019-04-29 16:49:11 -0500

    Subiendo practica 29/04/2019

---
+++ costomatricula/src/costomatricula/Principal.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package costomatricula;
+import porcentajes.Porcentajes;
+import java.util.Scanner;
+/**
+ *
+ * @author juanyanza11
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner (System.in);
+       
+                
+        String lugar;
+        int edad, estado, cargas;
+        double costoi = 1330;
+        double matF, mat;
+        double d1, d2, d3, d4, tramite;
+        // PEDIR DATOS
+        
+        System.out.println("Ingrese su localidad o ciudad");
+            lugar = entrada.nextLine();
+        System.out.println("Ingrese su edad");
+            edad = entrada.nextInt();
+        System.out.println("Ingrese su estado civil\n1.Casado\n2.Soltero");
+            estado=entrada.nextInt();
+        System.out.println("Tiene usted cargas familiares \n1.Si\n2.No");
+            cargas=entrada.nextInt();
+        // Importacion de variables de paquete dos
+        double descL = Porcentajes.porcentajeL;
+        double descE = Porcentajes.porcentajeE;
+        double descEc= Porcentajes.porcentajeEc;
+        double descCf= Porcentajes.porcentajeCf;
+        double descT= Porcentajes.porcentajeT;
+        // Condiciones de descuento
+        
+        if ("Loja".equals(lugar) || "Zamora".equals(lugar)){
+            
+            d1=descL;
+            
+        }else {
+            d1 = 0;
+        
+        }
+        
+       if (edad>=17 && edad<20){
+       
+           d2=descE;
+       }else{
+           d2=0;
+       }
+       if(estado==1){
+       
+           d3=descEc;
+           
+       }else{
+       
+           d3=0;
+       }
+       if(cargas==1){
+       
+           d4= descCf;
+       }else{
+       
+           d4=0;
+       }
+       
+       tramite = (1330-d1-d2-d3-d4)*0.02;
+       
+       matF=1330-d1-d2-d3-d4+tramite;
+         
+       //MUESTRA DE MATRÍCULA
+               
+       System.out.printf("El costo de su matricula equivale a: %.3f\n",matF);
+    }
+    
+}
---
+++ costomatricula/src/porcentajes/Porcentajes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package porcentajes;
+    
+/**
+ *
+ * @author ADMIN-HP
+ */
+public class Porcentajes {
+    
+    public static double porcentajeL = 1330*0.2;
+    public static double porcentajeE= 1330*0.1;
+    public static double porcentajeEc= 1330*0.05;
+    public static double porcentajeCf= 1330*0.03;
+    public static double porcentajeT= 0.02;
+    
+            
+}
